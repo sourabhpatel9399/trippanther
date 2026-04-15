05424346f58ebee5a4563b8c20c10df53a4b3773
sourabhpatel9399 <sourabhpatel7470@gmail.com>
2026-04-15 19:57:59 +0530

    k

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import {useNavigate } from "react-router-dom";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
